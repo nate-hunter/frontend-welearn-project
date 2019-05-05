960d3c3412962e2ebb7aed228846ae1a89acd257
Nate <github email address>
2019-05-05 15:03:20 -0400

    basic redux setup

---
+++ src/reducers/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+
+}
+
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+
+    case: "LOAD_USER"
+      return state
+
+    default:
+      return state
+  }
+}
+
+export default reducer;
